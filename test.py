5181fb6199b8fb6c3dbcc7bf46b542b51a274cdd
LukasDrsman <lukaskodr@gmail.com>
2021-12-31 17:57:54 +0100

    Add time-varying vector fields

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 """
-    A simple vector field plot.
+    A simple, time-varying vector field plot.
 
-    u(x,y,z) represents the i-th component of the vector,
-    v(x,y,z) represents the j-th component of the vector,
-    w(x,y,z) represents the k-th component of the vector,
+    u(x,y,z,t) represents the i-th component of the vector,
+    v(x,y,z,t) represents the j-th component of the vector,
+    w(x,y,z,t) represents the k-th component of the vector,
     thus,
-    
-        V(x,y,z) = u(x,y,z)i + v(x,y,z)j + w(x,y,z)k,
+
+        V(x,y,z,t) = u(x,y,z,t)i + v(x,y,z,t)j + w(x,y,z,t)k,
 
     where i, j, k are the unit vectors to the xyz coordinate system.
 """
@@ -15,13 +15,13 @@ from plot import Figure
 from vectorfield import *
 import numpy as np
 
-def u(x,y,z):
-    return y / 10
-def v(x,y,z):
-    return (y - x) / 10
-def w(x,y,z):
-    return 0*z
+def u(x,y,z,t):
+    return 0*x
+def v(x,y,z,t):
+    return 0.1*np.sin(t + x)
+def w(x,y,z,t):
+    return 0*x
 
 fig = Figure(800, 800)
-fig.add(VectorFieldCH(u, v, w, 5))
+fig.add(VectorFieldT(u, v, w, 4, 0, 2*np.pi, 0.05))
 fig.show()
--- vectorfield.py
+++ vectorfield.py
@@ -2,33 +2,7 @@ import numpy as np
 import OpenGL.GL as gl
 import OpenGL.GLU as glu
 
-class VectorFieldC:
-    def __init__(self, u, v, w, d, dz):
-
-        space = np.linspace(-0.5,0.5,d)
-        x, y, z = np.meshgrid(space, space, np.linspace(-dz,dz,d))
-
-        self.u = u(x, y, z)
-        self.v = v(x, y, z)
-        self.w = w(x, y, z)
-        self.m = np.sqrt(self.u**2 + self.v**2 + self.w**2)
-        self.m = self.m / np.max(self.m)
-        self.x = x
-        self.y = y
-        self.z = z
-
-    def render(self):
-        gl.glLineWidth(2.0)
-        gl.glBegin(gl.GL_LINES)
-        for i in range(self.x.shape[0]):
-            for j in range(self.x.shape[1]):
-                for k in range(self.x.shape[2]):
-                    gl.glColor3f(self.m[i,j,k], 0.5, 1 - self.m[i,j,k])
-                    gl.glVertex3f(self.x[i,j,k], self.y[i,j,k], self.z[i,j,k])
-                    gl.glVertex3f(self.x[i,j,k] + self.u[i,j,k], self.y[i,j,k] + self.v[i,j,k], self.z[i,j,k] + self.w[i,j,k])
-        gl.glEnd()
-
-class VectorFieldCH:
+class VectorField:
     def __init__(self, u, v, w, d):
 
         space = np.linspace(-0.5,0.5,d)
@@ -63,3 +37,53 @@ class VectorFieldCH:
                     gl.glVertex3f(self.x[i,j,k] + self.u[i,j,k], self.y[i,j,k] + self.v[i,j,k], self.z[i,j,k] + self.w[i,j,k])
 
         gl.glEnd()
+
+class VectorFieldT:
+    def __init__(self, u, v, w, d, ti, tf, dt):
+
+        space = np.linspace(-0.5,0.5,d)
+        x, y, z = np.meshgrid(space, space, space)
+
+        self.u = u
+        self.v = v
+        self.w = w
+
+        self.x = x
+        self.y = y
+        self.z = z
+
+        self.t = ti
+        self.T = (ti, tf, dt)
+
+    def render(self):
+        u = self.u(self.x, self.y, self.z, self.t)
+        v = self.v(self.x, self.y, self.z, self.t)
+        w = self.w(self.x, self.y, self.z, self.t)
+        m = np.sqrt(u**2 + v**2 + w**2)
+        if np.max(m) != 0:
+            m = m / np.max(m)
+
+        gl.glLineWidth(2.0)
+        gl.glBegin(gl.GL_LINES)
+        for i in range(self.x.shape[0]):
+            for j in range(self.x.shape[1]):
+                for k in range(self.x.shape[2]):
+                    gl.glColor3f(m[i,j,k], 0.5, 1 - m[i,j,k])
+                    gl.glVertex3f(self.x[i,j,k], self.y[i,j,k], self.z[i,j,k])
+                    gl.glVertex3f(self.x[i,j,k] + u[i,j,k], self.y[i,j,k] + v[i,j,k], self.z[i,j,k] + w[i,j,k])
+        gl.glEnd()
+
+        gl.glPointSize(4.0)
+        gl.glBegin(gl.GL_POINTS)
+        for i in range(self.x.shape[0]):
+            for j in range(self.x.shape[1]):
+                for k in range(self.x.shape[2]):
+                    gl.glColor3f(m[i,j,k], 0.5, 1 - m[i,j,k])
+                    gl.glVertex3f(self.x[i,j,k] + u[i,j,k], self.y[i,j,k] + v[i,j,k], self.z[i,j,k] + w[i,j,k])
+
+        gl.glEnd()
+
+        if self.t < self.T[1]:
+            self.t += self.T[2]
+        else:
+            self.t = self.T[0]
